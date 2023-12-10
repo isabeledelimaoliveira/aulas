894d6dac28541c43632da6a2e25cdf89a57fcf75
isabeledelimaoliveira <oisalima27@gmail.com>
2023-12-10 12:56:12 -0300

    finalizando calculadora

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,3 +1,49 @@
-function name(params) {
-    
+var valor1;
+var valor2;
+var operador;
+
+function preencheValor(valor) {
+    var input = document.getElementById("tela");
+    if (input.value == "0") {
+        input.value = valor;
+    } else {
+        input.value = input.value + valor;
+    }
 }
+
+function limparTela() {
+    document.getElementById("tela").value = ""
+}
+function operacao(op) {
+    var input = document.getElementById("tela");
+    valor1= input.value;
+    operador = op;
+    limparTela();
+
+}
+function calcula() {
+    var input = document.getElementById("tela");
+    valor2 = input.value;
+    switch (operador) {
+        case "+": input.value  = (parseFloat(valor1) + parseFloat(valor2));
+            break;
+        case "-": input.value  = (parseFloat(valor1) - parseFloat(valor2));
+            break;
+        case "*": input.value  = (parseFloat(valor1) * parseFloat(valor2));
+            break;
+        case ":": input.value  = (parseFloat(valor1) / parseFloat(valor2));
+            break;
+        case "%": input.value  = ((parseFloat(valor2) * parseFloat(valor1)) / 100);
+            break;
+
+    }
+}
+
+function porcentagem() {
+    operador = "%";
+    calcula();
+}
+
+
+
+
